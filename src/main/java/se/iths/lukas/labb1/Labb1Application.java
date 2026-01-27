eb670d8f58df27bc1d3975979ae259aaf2a19ac3
lewkus-dev <lukas.hjartat@gmail.com>
2026-01-27 10:20:15 +0100

    första commit

---
+++ src/main/java/se/iths/lukas/labb1/Labb1Application.java
@@ -0,0 +1,13 @@
+package se.iths.lukas.labb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/lukas/labb1/Labb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.lukas.labb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
