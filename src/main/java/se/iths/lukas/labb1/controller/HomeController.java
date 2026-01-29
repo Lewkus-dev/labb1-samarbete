85a3129284d5a8e679804249d305246f0d36a113
kevindbois <kevin.dbois@iths.se>
2026-01-29 13:38:24 +0100

    bytte namn till kevin från luk

--- src/main/java/se/iths/lukas/labb1/controller/HomeController.java
+++ src/main/java/se/iths/lukas/labb1/controller/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     //home controller
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("name", "Luk");
+        model.addAttribute("name", "Kevin");
         return "home";
     }
 }
